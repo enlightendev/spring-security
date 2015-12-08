f1555ed7dd67a504ad28e961258a114d3475cfac
EnlightenDev <enlightendevio@gmail.com>
2015-12-08 14:55:32 -0500

    misc + travis

--- OAuthAuthorizationServer/src/main/java/io/enlightendev/oauthserver/config/OAuth2AuthorizationServerConfig.java
+++ OAuthAuthorizationServer/src/main/java/io/enlightendev/oauthserver/config/OAuth2AuthorizationServerConfig.java
@@ -1,12 +1,16 @@
 package io.enlightendev.oauthserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  *
@@ -18,9 +22,19 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public JdbcTokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(this.authenticationManager);
+        endpoints
+                .authenticationManager(this.authenticationManager)
+                .tokenStore(tokenStore());
     }
 
     /**
--- OAuthResourceServer/README.md
+++ OAuthResourceServer/README.md
@@ -1,3 +1,9 @@
 ## About
 
 curl -H "Authorization: Bearer 2c7acb5e-da3b-4c4c-9823-3fb6633df739" localhost:9800/api/oauth2/ping
+
+
+
+https://github.com/spring-projects/spring-security-oauth/blob/master/tests/annotation/jdbc/src/main/java/demo/Application.java
+
+https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-secure-oauth2
